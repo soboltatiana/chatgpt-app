e412e6cfc4970df661b1b45738e985bf0da30a48
Tatiana Sobol <sobol.tatiana13@gmail.com>
2026-01-16 18:41:18 +0200

    feat: layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <main className="p-5">
+            <h1 className="text-4xl font-bold">About this app</h1>
+        </main>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,13 +23,26 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+        <header
+            className="text-white font-bold bg-blue-600 text-2xl p-2 mb-3 shadow-gray-700 flex">
+            <div className="flex flex-grow">
+                <Link href="/">GPT Chat</Link>
+                <Link href="/about" className="ml-5 font-light">
+                    About
+                </Link>
+            </div>
+        </header>
+        <div className="flex flex-col md:flex-row">
+            <div className="flex-grow">
+                {children}
+            </div>
+        </div>
+        </body>
+        </html>
+    );
 }
