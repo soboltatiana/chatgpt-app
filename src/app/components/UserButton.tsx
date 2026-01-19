d3055a38bbfcc6bba3edc185d9fa8b37d93f89c5
Tatiana Sobol <sobol.tatiana13@gmail.com>
2026-01-19 11:35:38 +0200

    feat: authentication

---
+++ src/app/components/UserButton.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
+function getFirstTwoCapitalLetters(str?: string | null) {
+    const match = (str || "").match(/[A-Z]/g);
+    return match ? match.slice(0, 2).join("") : "GT";
+}
+export function UserButton() {
+    const { data: session, status } = useSession();
+
+    return (
+        <div>
+            {status === "authenticated" && (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Avatar>
+                            { session?.user?.image && <AvatarImage src={session.user.image} />}
+                            <AvatarFallback>
+                                {getFirstTwoCapitalLetters(session?.user?.name)}
+                            </AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem
+                            onClick={() => {
+                                signOut();
+                            }}
+                        >
+                            Sign Out
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )}
+            {status === "unauthenticated" && (
+                <Button onClick={() => signIn()}>Sign in</Button>
+            )}
+        </div>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import {SessionProvider} from "@/app/components/SessionProvider";
+import {UserButton} from "@/app/components/UserButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,25 +26,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-        <header
-            className="text-white font-bold bg-blue-600 text-2xl p-2 mb-3 shadow-gray-700 flex">
-            <div className="flex flex-grow">
-                <Link href="/">GPT Chat</Link>
-                <Link href="/about" className="ml-5 font-light">
-                    About
-                </Link>
+        <SessionProvider>
+            <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+            <header
+                className="text-white font-bold bg-blue-600 text-2xl p-2 mb-3 shadow-gray-700 flex">
+                <div className="flex flex-grow">
+                    <Link href="/">GPT Chat</Link>
+                    <Link href="/about" className="ml-5 font-light">
+                        About
+                    </Link>
+                </div>
+                <div>
+                    <UserButton />
+                </div>
+            </header>
+            <div className="flex flex-col md:flex-row">
+                <div className="flex-grow">
+                    {children}
+                </div>
             </div>
-        </header>
-        <div className="flex flex-col md:flex-row">
-            <div className="flex-grow">
-                {children}
-            </div>
-        </div>
-        </body>
-        </html>
+            </body>
+            </html>
+        </SessionProvider>
     );
 }
